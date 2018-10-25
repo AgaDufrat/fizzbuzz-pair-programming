46f3b19737c4917b4854ceb201ee86e6f8600d93
alexandernewton <alex.newton@digital.cabinet-office.gov.uk>
2018-10-25 10:41:16 +0100

    Fizzbuzz test complete

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,20 @@
-def fizzbuzz(number)
-  return 'fizz'
+def fizzbuzz(i)
+  #1.upto(101) do |i|
+    if i % 5 == 0 && i % 3 == 0
+      return "fizzbuzz"
+    elsif i % 5 == 0
+      return "buzz"
+    elsif i % 3 == 0
+      return "fizz"
+    else
+      i
+    end
+  end
+#end
+
+i = 0
+
+while i < 101
+puts fizzbuzz(i)
+i += 1
 end
